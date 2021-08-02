50460936b3176f75931738c601bdbdb47f56f030
rovers <rovers@program>
2021-08-02 23:13:35 +0300

    i remove :main alias and move code to program.sh - deps.edn is for libraries only

--- program.sh
+++ program.sh
@@ -11,7 +11,11 @@ repl(){
 }
 
 main(){
-  clojure -M:main
+  clojure \
+    -J-Dclojure.core.async.pool-size=1 \
+    -J-Dclojure.compiler.direct-linking=false \
+    -M -m rovers.main
 }
+
 
 "$@"
