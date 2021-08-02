4e13dd2172b6210132520a1567ec86e55e7736ea
rovers <rovers@program>
2021-08-02 23:17:49 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -26,4 +26,30 @@ uberjar(){
   mv out/rovers.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting compilation..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar rovers.standalone.jar \
+    --name "rovers" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments rovers.main \
+    --app-version "1" \
+    $J_ARG
+}
+
 "$@"
