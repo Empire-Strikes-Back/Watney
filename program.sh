10807353731c7597995ec1e59895214057138412
rovers <rovers@program>
2021-09-01 20:47:03 +0300

    i use zazu library to generate identicon

--- program.sh
+++ program.sh
@@ -16,6 +16,12 @@ main(){
 uberjar(){
 
   clojure \
+    -X:identicon zazu.core/process \
+    :word '"rovers"' \
+    :filename '"out/identicon/icon.png"' \
+    :size 256
+
+  clojure \
     -X:uberjar genie.core/process \
     :main-ns rovers.main \
     :filename '"out/rovers.jar"' \
